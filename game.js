cfa3c260aaf68da202eb36ac9a78ef852f2c6ba9
Lonewolf <aaron0101727@hotmail.com>
2023-01-31 17:16:08 -0500

    creacion de las instancias de los objetos imagenes

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
-alert("hola")
+function initCanvas(){
+   let ctx = document.getElementById("mi_canvas").getContext("2d");
+   let BackgroundImage  = new Image();
+   let naveImage        = new Image();
+   let enemiespic1      = new Image();
+   let enemiespic2      = new Image();
+
+   BackgroundImage.src = ""
+}
