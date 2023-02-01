3e23a8ab35089ed24eb97052a1eccd6b6d2f0a7c
Lonewolf <aaron0101727@hotmail.com>
2023-02-01 16:11:47 -0500

    listo el esuchador de eventos para mover las nave con switch, esperando a solucionar el error de no moverse

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ function initCanvas(){
    const enemiespic1      = new Image();
    const enemiespic2      = new Image();
 
-   BackgroundImage.src = "./css/imagenes/fondo.jpg"
+   BackgroundImage.src = "./css/imagenes/fondo3.jpg"
    naveImage.src = "./css/imagenes/nave.png"
    enemiespic1.src = "./css/imagenes/enemigo1.gif"
    enemiespic2.src = "./css/imagenes/enemigo2.gif"
@@ -45,14 +45,28 @@ function initCanvas(){
         this.misiles = [];
 
         this.render = function(){
+            if(this.direccion === 'left'){
+                this.x -= 5;
+            }
+            else if(this.direccion === 'right'){
+                this.x += 5;
+            }
+            else if(this.direccion === 'downArrow'){
+                this.y += 5;
+            }
+            if(this.direccion === 'upArrow'){
+                this.y -= 5;
+            }
+
             ctx.fillStyle = this.bg;
-            ctx.drawImage(BackgroundImage, 10, 10);
+            ctx.drawImage(BackgroundImage, 0, 0);
             ctx.drawImage(naveImage, this.x, this.y, 100, 90);
         }
    }
 
    let launcher = new Launcher();
 
+   console.log(launcher)
 
    function animate(){
     ctx.clearRect(0, 0, canvas_W, canvas_H);
@@ -62,8 +76,75 @@ function initCanvas(){
 
    let animateInterval = setInterval(animate, 6);
 
+   document.addEventListener('keyup', function(event){
+    switch(event.keycode){
+        case teclas.LEFT:
+            launcher.x += 0;
+            launcher.direccion = '';
+        break;
+        case teclas.RIGHT:
+            launcher.x -= 0;
+            launcher.direccion;
+        break;
+        case teclas.UP:
+            launcher.y -= 0;
+            launcher.direccion = '';
+        break;
+        case teclas.DOWN:
+            launcher.y += 0;
+            launcher.direccion = '';
+    }
+    console.log(event)
+   })
 
+   document.addEventListener('keydown', function(event){
+    switch(event.keycode){
+        case teclas.UP:
+            launcher.direccion = 'upArrow'
+            if(launcher.y < canvas_H*.2 - 80){
+                launcher.y += 0;
+                launcher.direccion = '';
+            }
+        break;
+        case teclas.DOWN:
+            launcher.direccion = 'downArrow'
+            if(launcher.y > canvas_H*.2 - 110){
+                launcher.y -= 0;
+                launcher.direccion = '';
+            }
+        break;
+        case teclas.LEFT:
+            launcher.direccion = 'left'
+            if(launcher.x < canvas_W*.2 - 130){
+                launcher.x += 0;
+                launcher.direccion = '';
+            }
+        break;
+        case teclas.RIGHT:
+            launcher.direccion = 'right'
+            if(launcher.x > canvas_W - 110){
+                launcher.x -= 0;
+                launcher.direccion = '';
+            }
+        break;
+        case teclas.P:
+            this.location.reload();
+    }
+
+    console.log(event)
+
+   });
+
+  
 }
+
+const teclas = {
+    UP:38,
+    DOWN:40,
+    LEFT:37,
+    RIGHT:39,
+    P:80
+};
 
 window.addEventListener('load', function(event) {
     initCanvas()
